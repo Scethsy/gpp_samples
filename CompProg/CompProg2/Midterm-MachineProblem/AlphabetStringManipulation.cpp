fc1d9fa51e7af6728409fe2da34436e6389f7ad3
reginpv <reginpv@gmail.com>
2025-02-12 22:27:59 +0800

    alphabet string manipulatio

---
+++ CompProg/CompProg2/Midterm-MachineProblem/AlphabetStringManipulation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+  string word;
+  cout << "Enter a word: ";
+  cin >> word;
+
+  cout << "Original string: " << word << endl;
+
+  for (int i = 0; i < word.length(); i++) {
+    if (word[i] == 'z') {
+      word[i] = 'a';
+    } else if (word[i] == 'Z') {
+      word[i] = 'A';
+    } else if ((word[i] >= 'a' && word[i] < 'z') || (word[i] >= 'A' && word[i] < 'Z')) {
+      word[i]++;
+    }
+  }
+
+  cout << "New String: " << word << endl;
+
+  return 0;
+}
